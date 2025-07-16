03919c784be0247fbdd6e424228cc98f447ec98c
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-07-16 22:26:30 +0500

    Added Filter Product Functionality

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -8,13 +8,69 @@ const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState("");
+
+  const toogleCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const toogleSubCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setSubCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setSubCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const applyFilter = () => {
+    let productsCopy = products.slice();
+
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        category.includes(item.category)
+      );
+    }
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        subCategory.includes(item.subCategory)
+      );
+    }
+
+    setFilterProducts(productsCopy);
+  };
 
   useEffect(() => {
-    setFilterProducts(products);
-  }, []);
+    applyFilter();
+  }, [category, subCategory]);
+
+  let sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+    switch (sortType) {
+      case "low-high":
+        setFilterProducts(fpCopy.sort((a, b) => a.price - b.price));
+        break;
+      case "high-low":
+        setFilterProducts(fpCopy.sort((a, b) => b.price - a.price));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+  };
+
+  useEffect(() => {
+    sortProduct();
+  }, [sortType]);
 
   return (
-    <div class="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       <div className="min-w-60">
         <p
           onClick={() => setShowFilter(!showFilter)}
@@ -38,13 +94,31 @@ const Collection = () => {
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <label className="flex gap-2">
-              <input className="w-3" type="checkbox" value="Men" /> Men
+              <input
+                className="w-3"
+                type="checkbox"
+                value="Men"
+                onChange={toogleCategory}
+              />{" "}
+              Men
             </label>
             <label className="flex gap-2">
-              <input className="w-3" type="checkbox" value="Women" /> Women
+              <input
+                className="w-3"
+                type="checkbox"
+                value="Women"
+                onChange={toogleCategory}
+              />{" "}
+              Women
             </label>
             <label className="flex gap-2">
-              <input className="w-3" type="checkbox" value="Kids" /> Kids
+              <input
+                className="w-3"
+                type="checkbox"
+                value="Kids"
+                onChange={toogleCategory}
+              />{" "}
+              Kids
             </label>
           </div>
         </div>
@@ -57,30 +131,49 @@ const Collection = () => {
           <p className="mb-3 text-sm font-medium">TYPE</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <label className="flex gap-2">
-              <input className="w-3" type="checkbox" value="Topwear" /> Topwear
+              <input
+                className="w-3"
+                type="checkbox"
+                value="Topwear"
+                onChange={toogleSubCategory}
+              />{" "}
+              Topwear
             </label>
             <label className="flex gap-2">
-              <input className="w-3" type="checkbox" value="Bottomwear" />
+              <input
+                className="w-3"
+                type="checkbox"
+                value="Bottomwear"
+                onChange={toogleSubCategory}
+              />
               Bottomwear
             </label>
             <label className="flex gap-2">
-              <input className="w-3" type="checkbox" value="Winterwear" />
+              <input
+                className="w-3"
+                type="checkbox"
+                value="Winterwear"
+                onChange={toogleSubCategory}
+              />
               Winterwear
             </label>
           </div>
         </div>
       </div>
-      <div class="flex-1">
-        <div class="flex justify-between text-base sm:text-2xl mb-4">
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title text1="ALL" text2="COLLECTIONS" />
-          <select class="border-2 border-gray-300 text-sm px-2">
+          <select
+            onChange={(e) => setSortType(e.target.value)}
+            className="border-2 border-gray-300 text-sm px-2"
+          >
             <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
           </select>
         </div>
 
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
           {filterProducts.map((item, index) => (
             <ProductItem
               key={index}
